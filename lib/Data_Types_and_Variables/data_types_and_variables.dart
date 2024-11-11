2aa31b3b5c9af3ba051ba342f0e5fc09f570138f
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2024-11-11 12:51:55 +0600

    first commit

---
+++ lib/Data_Types_and_Variables/data_types_and_variables.dart
@@ -0,0 +1,13 @@
+void main() {
+  String name = "Shahin";
+  int age = 24;
+  bool male = true;
+  List<String> fruits = ["mango", "apple", "banana"];
+  Map<String, dynamic> myData = {
+    "name": name,
+    "age": age,
+    "male": male,
+    "fruits": fruits,
+  };
+  print(myData);
+}

29ca8a774fbb06a93ad157e428d69f97276e27cc
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2024-11-11 13:27:05 +0600

    List and Map Data Types in Dart

---
+++ lib/List_and_Map_Data_Types/list_and_map_data_types.dart
@@ -0,0 +1,27 @@
+void main() {
+  String name = "Shahin";
+  int age = 24;
+  bool male = true;
+  List<String> fruits = ["mango", "apple", "banana"];
+  Map<String, dynamic> myData = {
+    "name": name,
+    "age": age,
+    "male": male,
+    "fruits": fruits,
+  };
+  fruits.add("grapes"); // add single item
+  fruits.addAll(["black grapes", "white grapes"]); //addAll have add many more item
+  fruits.sort();
+  bool val = fruits.contains("mango");
+  var result = fruits.where((element) => element == "apple" || element == "banana").toList();
+  // print(result);
+  // print(val);
+  // print(fruits);
+
+  print(myData["age"]);
+  print(myData.keys.toList());
+  print(myData.values);
+  print(myData.containsValue("apple"));
+ var bal = myData.containsKey("name");
+ print(bal);
+}
